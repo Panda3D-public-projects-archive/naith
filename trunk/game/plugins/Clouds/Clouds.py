09ed040d535b0dca10f9a1aa89421d65264af70c
aurilliance <aurilliance@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-04-29 22:40:51 +0000

    Clouds are a little better now. Still some glitches

--- trunk/game/plugins/Clouds/Clouds.py
+++ trunk/game/plugins/Clouds/Clouds.py
@@ -15,25 +15,40 @@
 import os, random
 #from pandac.PandaModules import Point2, Vec3, Vec4, NodePath, CardMaker, Shader, ColorBlendAttrib, Texture
 from pandac.PandaModules import *
+from direct.task.Task import Task
+from direct.interval.IntervalGlobal import *
 
 class CloudObj():
   """An object to represent a single cloud cluster"""
-  def __init__(self, filename, splat_texture, position, softness=0.5):
+  def __init__(self, filename, splat_texture, position, softness=0.5, visibility=0.5):
     
     # Each cloud has a .egg file and a splat texture associated with it
     self.model = loader.loadModel(filename)
     self.splat_texture = splat_texture
     
     # Attach this cloud's node and create the list of sprites
-    self.cloud_node = render.attachNewNode("cloud")
+    self.cloud_node = base.cam.attachNewNode("cloud"+str(random.random()))
+    self.cloud_node.setCompass()
     self.sprites = []
     
+    # The cloud has a value that represents how formated or dissolved it is
+    self.visibility = visibility
+    
+    # This is used for the fading in and out
+    self.longest_dist = 0
+    
     # Set the cloud's position
-    self.cloud_node.setPos(position)
+    self.cloud_node.setPos(base.cam, position)
     
     # Note - stratus type clouds use higher softness values
     # Cumulus clouds use lower softness values
     self.softness = softness
+  
+  def set_visibility(self, vis):
+    """Sets the visibility of the cloud, the futher away from the center each sprite is, the less visibile"""
+    self.visibility = vis
+    for sprite in self.sprites:
+      sprite[1].setAlphaScale((1.0-sprite[0])*vis)
   
   def _getmiddle(self, points):
     """Returns the center of a sequence of 3-space points"""
@@ -57,31 +72,43 @@ class CloudObj():
       sprite_midpoint = self._getmiddle(tight_bounds)
       
       #Set the size of the billboard, !roughly based on the size of the box
-      cm.setFrame(tight_bounds[0].getX(), tight_bounds[1].getY(), tight_bounds[0].getX(), tight_bounds[1].getZ())
+      cm.setFrame(tight_bounds[0].getX(), tight_bounds[1].getY(), tight_bounds[0].getZ(), tight_bounds[1].getZ())
       
-      # Choose a texture splat image based on the softness value XXX Not with the following lines commented out
-      gauss_softness = random.gauss(self.softness, 0.3)
-      for i in range(1,16):
-        if (gauss_softness <= i*.0625) and (gauss_softness >= (i-1)*.0625): # XXX Use this line to use gaussian random textures
-        #if (self.softness <= i*.0625) and (self.softness >= (i-1)*.0625): # XXX Use this line to NOT use gaussian random textures
-          # i is the texture we're after. Note i=0 will be the bottom left 
-          # texture, i=15 will be the top-right texture
-          row,column = divmod(i, 4)
-          cm.setUvRange((row*0.25, column*0.25), ((row+1)*0.25, ((column+1)*0.25)))
+      # Choose a texture splat image based on the softness value
+      tmpsoftness = random.gauss(self.softness, 0.1); num = 0;
+      if tmpsoftness <= 1*.0625: pass
+      elif tmpsoftness <= 2*.0625: num = 1
+      elif tmpsoftness <= 3*.0625: num = 2
+      elif tmpsoftness <= 4*.0625: num = 3
+      elif tmpsoftness <= 5*.0625: num = 4
+      elif tmpsoftness <= 6*.0625: num = 5
+      elif tmpsoftness <= 7*.0625: num = 6
+      elif tmpsoftness <= 8*.0625: num = 7
+      elif tmpsoftness <= 9*.0625: num = 8
+      elif tmpsoftness <= 10*.0625: num = 9
+      elif tmpsoftness <= 11*.0625: num = 10
+      elif tmpsoftness <= 12*.0625: num = 11
+      elif tmpsoftness <= 13*.0625: num = 12
+      elif tmpsoftness <= 14*.0625: num = 13
+      elif tmpsoftness <= 15*.0625: num = 14
+      else: num = 15
+      row,column = divmod(num, 4)
+      cm.setUvRange((row*0.25, column*0.25), ((row+1)*0.25, ((column+1)*0.25)))
       
       # Create the sprite
-      sprite = render.attachNewNode(cm.generate())
+      sprite = self.cloud_node.attachNewNode(cm.generate())
       sprite.setPos(self.cloud_node, sprite_midpoint)
       sprite.setTexture(self.splat_texture)
       sprite.setBillboardPointEye()
       #sprite.setBillboardAxis()
-      #sprite.setTwoSided(True)
+      sprite.setTwoSided(True)
       #sprite.setBin('background', 20)
       sprite.setTransparency(TransparencyAttrib.MDual)
       sprite.setLightOff()
       
       # Calc the distance from the center of the cloud to this sprite
       distance = Vec3(sprite.getPos(self.cloud_node)).length()
+      if self.longest_dist < distance: self.longest_dist = distance
       
       self.sprites.append([distance, sprite])
     
@@ -89,7 +116,13 @@ class CloudObj():
     self.model.removeNode()
     
     # Re-sort the sprites from closest to the core->furthest from the core
+    # While we're at it, we pre-calc the normalised distances
     self.sprites = sorted(self.sprites)
+    for sprite in self.sprites:
+      sprite[0] = sprite[0] / self.longest_dist
+    
+    # Set the visibility of the cloud
+    self.set_visibility(self.visibility)
         
 
 class Clouds:
@@ -102,9 +135,12 @@ class Clouds:
     xmlcloudlist = [x for x in xml.findall('cloud')]
     cloudsplattexture = loader.loadTexture(os.path.join(basePath, xml.find('splat').get('fname')))
     
-    # See if the user is requesting a cloudbox or not
+    # XXX See if the user is requesting a cloudbox or a position
+    # Needs to be handled much better than this
     self.cloudbox = None
+    self.cloudpos = None
     xmlcloudbox = xml.find('range')
+    xmlcloudpos = xml.find('pos')
     if xmlcloudbox != None:
       self.cloudbox = (Point3(int(xmlcloudbox.get('x1')),
                               int(xmlcloudbox.get('y1')),
@@ -112,6 +148,10 @@ class Clouds:
                        Point3(int(xmlcloudbox.get('x2')),
                               int(xmlcloudbox.get('y2')),
                               int(xmlcloudbox.get('z2'))))
+    if xmlcloudpos != None:
+      self.cloudpos = Point3(int(xmlcloudpos.get('x')),
+                             int(xmlcloudpos.get('y')),
+                             int(xmlcloudpos.get('z')))
      
     
     # Iterate over each of the requested clouds
@@ -132,6 +172,14 @@ class Clouds:
         self.cloudlist.append(cloud)
       else:
         # Default the cloud to (0,0,0)
-        cloud = CloudObj(os.path.join(basePath, filename), cloudsplattexture, Point3(0,0,0), softness)
+        cloud = CloudObj(os.path.join(basePath, filename), cloudsplattexture, self.cloudpos, softness)
         cloud.generate_sprites()
         self.cloudlist.append(cloud)
+      
+      
+      # Create a testing lerp of the cloud's vis
+      cloudfadeout = LerpFunc(cloud.set_visibility, 10, 1, 0, 'easeInOut')
+      cloudfadein = LerpFunc(cloud.set_visibility, 10, 0, 1, 'easeInOut')
+      cloudsequence = Sequence(cloudfadeout,cloudfadein)
+      cloudsequence.loop()
+         
