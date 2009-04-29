b415ac738f58f24049c83cfa184e6110379d6a70
niertie1 <niertie1@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-04-29 12:52:41 +0000

    Add support for PlaneGeoms, and support for <instance> tags

--- trunk/game/plugins/PhysicsObject/PhysicsObject.py
+++ trunk/game/plugins/PhysicsObject/PhysicsObject.py
@@ -12,58 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
 import os.path
 from pandac.PandaModules import *
-import direct.directbase.DirectStart
-
 
 class PhysicsObject:
-  """Provides a simple physics object capability - replaces all of a specific IsA in a scene with a specific mesh and specific physics capabilities. Initialises such objects with simulation off, so they won't move until they itneract with the player or an AI somehow - needed to restrict computation but means such objects must be positioned very accuratly in the level."""
+  """Provides a simple physics object capability - replaces all of a specific IsA in a scene with a specific mesh and specific physics capabilities. Initialises such objects with simulation off, so they won't move until they itneract with the player or an AI somehow - needed to restrict computation but means such objects must be positioned very accuratly in the level.
+  It could also contain a bunch of <instance> tags, that way you can specify positions yourself instead of doing it in the world model."""
   def __init__(self,manager,xml):
     basePath = manager.get('paths').getConfig().find('objects').get('path')
     ode = manager.get('ode')
     self.things = [] # Tuple of (mesh/body/collider)
-    
-    # Find all instances of the object to obtain...
-    level = manager.get(xml.find('isa').get('source'))
-    toMake = level.getByIsA(xml.find('isa').get('name'))
-    for make in toMake:
-      # Load the mesh, parent to render...
-      filename = os.path.join(basePath,xml.find('mesh').get('filename'))
-      model = loader.loadModel(filename)
-      model.reparentTo(render)
-      model.setShaderAuto()
+    phys = xml.find('physics')
+    pType = phys.get('type').lower()
 
-      # Move it to the correct location/orientation...
-      model.setPosQuat(make.getPos(render),make.getQuat(render))
-      
-      # Create the mass object for the physics...
-      body = OdeBody(ode.getWorld())
-      mass = OdeMass()
-      phys = xml.find('physics')
-      pType = phys.get('type')
-      if pType=='sphere':
-        mass.setSphereTotal(float(phys.get('mass')), float(phys.get('radius')))
-      elif pType=='box':
-        mass.setBoxTotal(float(phys.get('mass')), float(phys.get('lx')), float(phys.get('ly')), float(phys.get('lz')))
-      elif pType=='cylinder':
-        mass.setCylinderTotal(float(phys.get('mass')), 3, float(phys.get('radius')), float(phys.get('height')))
-      elif pType=='capsule':
-        mass.setCapsuleTotal(float(phys.get('mass')), 3, float(phys.get('radius')), float(phys.get('height')))
-      elif pType=='mesh':
-        # Need some way of calculating/obtaining an inertial tensor - currently using a box centered on the object with the dimensions of the collision meshes bounding axis aligned box...
-        colMesh = loader.loadModel(os.path.join(basePath,phys.get('filename')))
-        low, high = colMesh.getTightBounds()
-        mass.setBoxTotal(float(phys.get('mass')), high[0]-low[0], high[1]-low[1], high[2]-low[2])
-      else:
-        raise Exception('Unrecognised physics type')
-      
-      body.setMass(mass)
-      body.setPosition(make.getPos(render))
-      body.setQuaternion(make.getQuat(render))
-      body.disable() # To save computation until the player actually interacts with 'em. And stop that annoying jitter.
+    # Find all instances of the object to obtain...
+    toMake = []
+    isa = xml.find('isa')
+    if isa != None:
+      level = manager.get(isa.get('source'))
+      toMake = level.getByIsA(isa.get('name'))
+
+    for inst in xml.findall('instance'):
+      # Find <instance> tags that can be used to create instances of the physics object.
+      make = NodePath('physicsObject')
+      make.setPos(  float(inst.get('x', '0')), float(inst.get('y', '0')), float(inst.get('z', '0')))
+      make.setHpr(  float(inst.get('h', '0')), float(inst.get('p', '0')), float(inst.get('r', '0')))
+      make.setScale(float(inst.get('sx','1')), float(inst.get('sy','1')), float(inst.get('sz','1')))
+      toMake.append(make)
+
+    for make in toMake:
+      if xml.find('mesh') != None:
+        # Load the mesh, parent to render...
+        filename = os.path.join(basePath,xml.find('mesh').get('filename'))
+        model = loader.loadModel(filename)
+        model.reparentTo(render)
+        model.setShaderAuto()
+        if pType=='mesh':
+          # Need some way of calculating/obtaining an inertial tensor - currently using a box centered on the object with the dimensions of the collision meshes bounding axis aligned box...
+          colMesh = loader.loadModel(os.path.join(basePath,phys.get('filename')))
 
       # Create the collision object...
       if pType=='sphere':
@@ -76,15 +62,56 @@ class PhysicsObject:
         col = OdeCappedCylinderGeom(ode.getSpace(), float(phys.get('radius')), float(phys.get('height')))
       elif pType=='mesh':
         col = OdeTriMeshGeom(ode.getSpace(), OdeTriMeshData(colMesh,True))
+      elif pType=='plane':
+        col = OdePlaneGeom(ode.getSpace(), float(phys.get('a')), float(phys.get('b')), float(phys.get('c')), -float(phys.get('d')))
 
-      col.setBody(body)
+      # Weirdness! We can't move a Plane, so we'll have to bake the transform onto the plane equation.
+      # We can do that easily thanks to panda's Plane class.
+      if pType=='plane':
+        # Remember that the D is inverted in ODE compared to Panda.
+        plane = Plane(col.getParams())
+        plane.setW(-plane.getW())
+        plane.xform(make.getNetTransform().getMat())
+        plane.setW(-plane.getW())
+        col.setParams(plane)
+      else:
+        col.setPosition(make.getPos(render))
+        col.setQuaternion(make.getQuat(render))
+
       surface = phys.get('surface')
       ode.getSpace().setSurfaceType(col,ode.getSurface(surface))
 
-      # Tie everything together, arrange for damping...
-      ode.regBodySynch(model,body)
-      self.things.append((model,body,col))
+      if xml.find('mesh') != None:
+        # Move it to the correct location/orientation...
 
-      damp = xml.find('damping')
-      if damp!=None:
-        ode.regDamping(body,float(damp.get('linear')),float(damp.get('angular')))
+        model.setPosQuat(make.getPos(render),make.getQuat(render))
+
+        # Create the mass object for the physics...
+        body = OdeBody(ode.getWorld())
+        col.setBody(body)
+        mass = OdeMass()
+        if pType=='sphere':
+          mass.setSphereTotal(float(phys.get('mass')), float(phys.get('radius')))
+        elif pType=='box':
+          mass.setBoxTotal(float(phys.get('mass')), float(phys.get('lx')), float(phys.get('ly')), float(phys.get('lz')))
+        elif pType=='cylinder':
+          mass.setCylinderTotal(float(phys.get('mass')), 3, float(phys.get('radius')), float(phys.get('height')))
+        elif pType=='capsule':
+          mass.setCapsuleTotal(float(phys.get('mass')), 3, float(phys.get('radius')), float(phys.get('height')))
+        elif pType=='mesh':
+          low, high = colMesh.getTightBounds()
+          mass.setBoxTotal(float(phys.get('mass')), high[0]-low[0], high[1]-low[1], high[2]-low[2])
+        else:
+          raise Exception('Unrecognised physics type')
+
+        body.setMass(mass)
+        body.setPosition(make.getPos(render))
+        body.setQuaternion(make.getQuat(render))
+        body.disable() # To save computation until the player actually interacts with 'em. And stop that annoying jitter.
+        damp = xml.find('damping')
+        if damp!=None:
+          ode.regDamping(body,float(damp.get('linear')),float(damp.get('angular')))
+
+        # Tie everything together, arrange for damping...
+        ode.regBodySynch(model,body)
+        self.things.append((model,body,col))
