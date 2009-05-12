7d9243324eaa5a28a8d8ba980234cbeba31c6952
thaines <thaines@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-05-12 19:17:33 +0000

    Weapon plugin made loading screen safe; loading screen added.

--- game/plugins/SimpleWeapon/SimpleWeapon.py
+++ game/plugins/SimpleWeapon/SimpleWeapon.py
@@ -30,33 +30,51 @@ from pandac.PandaModules import *
 class SimpleWeapon:
   """Provides a simple weapon system - not very sophisticaed, but good enough to test shooting things."""
   def __init__(self,manager,xml):
+    self.gunView = render.attachNewNode('gun-view')
+    self.ray = None
+
+    self.reload(manager,xml)
+
+
+  def destroy(self):
+    self.gunView.removeNode()
+
+    if self.ray!=None:
+      self.ray.destroy()
+
+
+  def reload(self,manager,xml):
     # Get the path to load weapons from...
     basePath = manager.get('paths').getConfig().find('weapons').get('path')
 
-    # Load the weapon...
-    meshPath = os.path.join(basePath,xml.find('egg').get('file'))
-    self.mesh = Actor()
-    self.mesh.loadModel(meshPath)
-    self.mesh.setShaderAuto()
+    # Variables to manage the firing state (Used G36 as reference for defaults.)...
+    bullet = xml.find('bullet')
+    if bullet!=None:
+      self.bulletRate = float(bullet.get('rate',1.0/12.5))
+      self.bulletSpeed = float(bullet.get('speed',920.0))
+      self.bulletWeight = float(bullet.get('mass',0.004))
+    else:
+      self.bulletRate = 1.0/12.5
+      self.bulletSpeed = 920.0
+      self.bulletWeight = 0.004
+
+    # Determine the weapon meshes path...
+    self.meshPath = os.path.join(basePath,xml.find('egg').get('file'))
 
     # Get the camera interface, so we can zoom in when the player aims...
     self.camera = manager.get(xml.find('camera').get('plugin'))
 
     # Create our gun node - both the gun and the ray used for shooting track this - allows for gun jitter, kick back etc...
     parent = xml.find('parent')
-    self.gunView = manager.get(parent.get('plugin')).getNode(parent.get('node')).attachNewNode('gun-view')
-
-    # Parent the mesh to the gun view...
-    self.mesh.reparentTo(self.gunView)
-
-    # Set the animation going...
-    self.state = False # False==casual, True==aim.
-    self.nextState = False
-    self.nextAni()
+    self.gunView.reparentTo(manager.get(parent.get('plugin')).getNode(parent.get('node')))
 
     # Create a ray cast to detect what the player is looking at... and what will be shot...
     self.space = manager.get('ode').getSpace()
-    self.ray = OdeRayGeom(50.0)
+
+    if self.ray!=None:
+      self.ray.destroy()
+
+    self.ray = OdeRayGeom(100.0)
     self.ray.setCategoryBits(BitMask32(0xfffffffe))
     self.ray.setCollideBits(BitMask32(0xfffffffe))
 
@@ -64,7 +82,7 @@ class SimpleWeapon:
     flash = xml.find('muzzle_flash')
     self.flashManager = manager.get(flash.get('plugin'))
     self.flashEffect = flash.get('effect')
-    self.flashBone = self.mesh.exposeJoint(None,"modelRoot",flash.get('bone'))
+    self.flashBone = flash.get('bone') # Will be swapped out for the actual node latter.
     self.flashPos = csp.getPos(flash.get('pos'))
 
     # Get all the stuff we need to do the bullet hit sparks effect...
@@ -75,73 +93,114 @@ class SimpleWeapon:
     # Create a quaternion that rotates +ve z to +ve y - used to point it in the weapon direction rather than up...
     self.zToY = Quat()
     self.zToY.setFromAxisAngle(-90.0,Vec3(1.0,0.0,0.0))
-
-    # Variables to manage the firing state...
+    
+    # State for the animation...
+    self.state = False # False==casual, True==aim.
+    self.nextState = False
+    
+    # Firing state...
     self.firing = False # True if the trigger is being held.
     self.triggerTime = 0.0 # How long the trigger has been held for, so we know when to eject ammo.
-    self.bulletRate = 1.0/12.5 # Used G36 as reference
-    self.bulletSpeed = 920.0 # Used G36 as reference - muzzle velocity, not bothering with air resistance.
-    self.bulletWeight = 0.004 # 5.56x45mm NATO
+
+
+  def postInit(self):
+    for i in self.postReload():
+      yield i
+
+  def postReload(self):
+    # Load the actor...
+    self.mesh = Actor()
+    yield
+    self.mesh.loadModel(self.meshPath)
+    yield
+
+    # Shader generator makes it shiny, plus we need it in the right places in the render graph...
+    self.mesh.setShaderAuto()
+    self.mesh.reparentTo(self.gunView)
+    self.mesh.hide()
+    yield
+
+    # Set its animation going... except we pause it until needed...
+    self.nextAni()
+    self.interval.pause()
+
+    # Gun flash requires an exposed bone...
+    self.flashBone = self.mesh.exposeJoint(None,"modelRoot",self.flashBone)
+    yield
+
+
+  def gunControl(self,task):
+    # Update the gun direction ray to follow the players view...
+    self.ray.setPosition(self.gunView.getPos(render))
+    self.ray.setQuaternion(self.zToY.multiply(self.gunView.getQuat(render)))
+
+    # If the gun is firing update the trigger time, if a bullet is ejected do the maths...
+    if self.firing:
+      dt = globalClock.getDt()
+      self.triggerTime += dt
+      while self.triggerTime>self.bulletRate:
+        self.triggerTime -= self.bulletRate
+        hit,pos,norm = ray_cast.nearestHit(self.space,self.ray)
+
+        # Create a muzzle flash effect...
+        self.flashManager.doEffect(self.flashEffect, self.flashBone, True, self.flashPos)
+
+        # Create an impact sparks effect...
+        if hit!=None:
+          # Calculate the reflection direction...
+          rd = self.ray.getDirection()
+          sparkDir = (norm * (2.0*norm.dot(rd))) - rd
+
+          # Convert the reflection direction into a quaternion that will rotate +ve z to the required direction...
+          try:
+            ang = -math.acos(sparkDir[2])
+          except:
+            print 'Angle problem', sparkDir
+            ang = 0.0
+          axis = Vec3(0.0,0.0,1.0).cross(sparkDir)
+          axis.normalize()
+          sparkQuat = Quat()
+          sparkQuat.setFromAxisAngleRad(ang,axis)
+
+          # Set it going...
+          self.sparksManager.doEffect(self.sparksEffect, render, False, pos, sparkQuat)
+
+        # Impart some energy on the object...
+        if hit!=None and hit.hasBody():
+          body = hit.getBody()
+
+          # Calculate the force required to supply the energy the bullet contains to the body...
+          force = self.bulletWeight*self.bulletSpeed/0.05
+
+          # Get the direction of travel of the bullet, multiply by force...
+          d = self.ray.getDirection()
+          d *= force
+
+          # If the object is asleep awaken it...
+          if not body.isEnabled():
+            body.enable()
+
+          # Add the force to the object...
+          body.addForceAtPos(d,pos)
+
+    return task.cont
+
+
+  def start(self):
+    # Make the gun visible...
+    self.mesh.show()
     
+    # Set the gun animation going...
+    self.interval.finish()
 
-    # Arrange a task to handle the gun shooting...
-    def gunControl(task):
-      # Update the gun direction ray to follow the players view...
-      self.ray.setPosition(self.gunView.getPos(render))
-      self.ray.setQuaternion(self.zToY.multiply(self.gunView.getQuat(render)))
+    # Weapon task - this primarily makes it shoot...
+    self.task = taskMgr.add(self.gunControl,'GunControl')
 
-      # If the gun is firing update the trigger time, if a bullet is ejected do the maths...
-      if self.firing:
-        dt = globalClock.getDt()
-        self.triggerTime += dt
-        while self.triggerTime>self.bulletRate:
-          self.triggerTime -= self.bulletRate
-          hit,pos,norm = ray_cast.nearestHit(self.space,self.ray)
 
-          # Create a muzzle flash effect...
-          self.flashManager.doEffect(self.flashEffect, self.flashBone, True, self.flashPos)
-
-          # Create an impact sparks effect...
-          if hit!=None:
-            # Calculate the reflection direction...
-            rd = self.ray.getDirection()
-            sparkDir = (norm * (2.0*norm.dot(rd))) - rd
-            
-            # Convert the reflection direction into a quaternion that will rotate +ve z to the required direction...
-            try:
-              ang = -math.acos(sparkDir[2])
-            except:
-              print 'Angle problem', sparkDir
-              ang = 0.0
-            axis = Vec3(0.0,0.0,1.0).cross(sparkDir)
-            axis.normalize()
-            sparkQuat = Quat()
-            sparkQuat.setFromAxisAngleRad(ang,axis)
-            
-            # Set it going...
-            self.sparksManager.doEffect(self.sparksEffect, render, False, pos, sparkQuat)
-          
-          # Impart some energy on the object...
-          if hit!=None and hit.hasBody():
-            body = hit.getBody()
-            
-            # Calculate the force required to supply the energy the bullet contains to the body...
-            force = self.bulletWeight*self.bulletSpeed/0.05
-
-            # Get the direction of travel of the bullet, multiply by force...
-            d = self.ray.getDirection()
-            d *= force
-
-            # If the object is asleep awaken it...
-            if not body.isEnabled():
-              body.enable()
-
-            # Add the force to the object...
-            body.addForceAtPos(d,pos)
-
-      return task.cont
-
-    taskMgr.add(gunControl,'GunControl')
+  def stop(self):
+    self.interval.pause()
+    self.mesh.hide()
+    taskMgr.remove(self.task)
 
 
   def nextAni(self):
@@ -153,6 +212,7 @@ class SimpleWeapon:
     self.mesh.pose(ani,0)
     self.interval = Sequence(self.mesh.actorInterval(ani),Func(self.nextAni))
     self.interval.start()
+
 
   def setAiming(self,s):
     if self.nextState!=s:
@@ -167,6 +227,7 @@ class SimpleWeapon:
       else: ani = 'aim_casual'
       transition = Sequence(self.mesh.actorInterval(ani),Func(wib))
       transition.start()
+
 
   def setFiring(self,s):
     self.firing = s
