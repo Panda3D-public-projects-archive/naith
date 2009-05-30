2398ddd1ffe7c0ac9c5520fe99aef7479e3e9816
thaines <thaines@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-05-30 15:42:08 +0000

    Portal discovery code

--- game/plugins/CullAABB/AABB.py
+++ game/plugins/CullAABB/AABB.py
@@ -20,10 +20,10 @@ import math
 class AABB:
   """Defines an axis aligned bounding box."""
   def __init__(self,low,high):
-    self.x = (low[0],high[0])
-    self.y = (low[1],high[1])
-    self.z = (low[2],high[2])
-    self.bounds = (self.x,self.y,self.z)
+    self.x = [low[0],high[0]]
+    self.y = [low[1],high[1]]
+    self.z = [low[2],high[2]]
+    self.bounds = [self.x,self.y,self.z]
 
     self.centre = (0.5*(self.x[0]+self.x[1]),0.5*(self.y[0]+self.y[1]),0.5*(self.z[0]+self.z[1]))
     self.volume = (self.x[1]-self.x[0]) * (self.y[1]-self.y[0]) * (self.z[1]-self.z[0])
@@ -65,7 +65,7 @@ class SetAABB:
       centre = 0
       volume = 0.0
       while centre+1<len(byDim) and volume<halfVolume:
-        volume += byDim[centre].voume
+        volume += byDim[centre].volume
         centre += 1
 
       options = (byDim[centre].bounds[dim][0]-aabbLambda, byDim[centre].centre[dim], byDim[centre].bounds[dim][1]+aabbLambda)
@@ -133,3 +133,108 @@ class SetAABB:
       else:
         res = self.high.within(node)
         if res!=None: return res
+
+
+
+class Portal:
+  """Defines a portal by its 4 vertices."""
+  def __init__(self):
+    self.verts = [(1.0,0.0,1.0),(-1.0,0.0,1.0),(-1.0,0.0,-1.0),(1.0,0.0,-1.0)]
+    self.aabb1 = None
+    self.aabb2 = None
+
+  def fromFace(self,aabb,dim,side):
+    """Setup the portal from a face of the given aabb - you specify the dim of the face, with side==False meaning the low side and side==True meaning the high side. Will be anti-clockwise looking at it from inside the cube."""
+    if side:
+      side = 1
+      inc = 1
+    else:
+      side = 0
+      inc = 2
+
+    # Define square2d, remap it to 3D coordinates based on dim and side...
+    square2d = [(0,0),(0,1),(1,1),(1,0)]
+    def incDim(base):
+      ret = [0,0,0]
+      ret[(dim+inc)%3] = base[0]
+      ret[(dim+2*inc)%3] = base[1]
+      ret[dim] = side
+      return ret
+    square3d = map(incDim,square2d)
+
+    # Extract the 4 coordinates...
+    self.verts = []
+    for index in square3d:
+      coord = map(lambda d: aabb.bounds[d][index[d]],xrange(3))
+      self.verts.append(coord)
+
+
+
+def findPortals(aabbs,overlap = 1e-3):
+  """Given a list of AABB's this finds all intersections and creates portals. To store the portals creates a variable in each aabb, portals = [[[],[]],[[],[]],[[],[]]] - first index is dimension, second index is low (0) and high (1), final list is all portals using that face. Returns the portals as a list. Will throw an error if the geometry is bad. Will modify the dimensions of the given aabb's to account for overlap."""
+  # Before we start add the portal variable to each aabb...
+  for aabb in aabbs:
+    aabb.portals = [[[],[]],[[],[]],[[],[]]]
+  
+  # We process each dimension seperatly - this first loop is over the dimensions...
+  for dim in xrange(3):
+    otherDim = [0,1,2]
+    del otherDim[dim]
+
+    # Iterate all aabbs and create a push event and pop event for each - push it on when you reach the minimum, pop it when you get to the maximum. (True,d,aabb) to push, (False,d,aabb) to pop...
+    events = []
+    for aabb in aabbs:
+      events.append((True,aabb.bounds[dim][0],aabb))
+      events.append((False,aabb.bounds[dim][1],aabb))
+
+    # Sort the events...
+    events.sort(key=lambda x: x[1])
+
+    # Iterate through the events in sequence - each time a aabb is pushed or popped check if it intercepts a face larger than it - if so add the relevant portal... (Partial interception is considered an error as it results in ambiguous behaviour. Multiple interception is also not allowed as its an entire face that intercepts from our point of view. (Larger face can have multiple intercepts of course.))
+    ret = []
+    state = dict() # Index by id of aabb's
+    for event in events:
+      if not event[0]:
+        # Pop event - remove its aabb from the state...
+        del state[id(event[2])]
+        
+      # Check event aabb against existing aabbs for being the smaller face...
+      done = False
+      for key,aabb in state.iteritems():
+        withinCount = 0
+        for od in otherDim:
+          if event[2].bounds[od][0]>aabb.bounds[od][0] and event[2].bounds[od][0]<aabb.bounds[od][1]:
+            withinCount += 1
+          if event[2].bounds[od][1]>aabb.bounds[od][0] and event[2].bounds[od][1]<aabb.bounds[od][1]:
+            withinCount += 1
+
+        if withinCount==4:
+          if done:
+            raise Exception('Double interception - each culling aabb face can only intrecept one other cube as a fully contained face')
+          done = True
+
+          # We have an interception - update the relevant aabb to have only the slightest overlap then create the portal and finally arrange for all the links...
+          if event[0]:
+            event[2].bounds[dim][0] = aabb.bounds[dim][1] - overlap
+            evSide = 0
+          else:
+            event[2].bounds[dim][1] = aabb.bounds[dim][0] + overlap
+            evSide = 1
+          
+          portal = Portal()
+          portal.fromFace(event[2],dim,not event[0])
+          ret.append(portal)
+
+          portal.aabb1 = event[2]
+          portal.aabb2 = aabb
+          event[2].portals[dim][evSide].append(portal)
+          aabb.portals[dim][(evSide+1)%2].append(portal)
+
+        elif withinCount!=0:
+          raise Exception('Partial interception - culling aabbs can not intecept at corners/edges due to undefinable behaviour - must only overlap with one face fully contained within another.')
+
+      if event[0]:
+        # Push event - add the events aabb to the state...
+        state[id(event[2])] = event[2]
+
+    return ret
