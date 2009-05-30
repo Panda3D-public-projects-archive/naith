219ae176acbbb74f01cc1e22408ef02331c9bf47
thaines <thaines@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-05-30 15:42:36 +0000

    F8 hides level; culling system works... sort of. Its looking like there are a whole bunch of issues though (Mostly when you walk through a portal.). (Only does static objects right now.)

--- trunk/game/plugins/CullAABB/CullAABB.py
+++ trunk/game/plugins/CullAABB/CullAABB.py
@@ -20,9 +20,33 @@ from AABB import *
 
 class CullAABB:
   def __init__(self,manager,xml):
+    self.bounds = []
+    self.portals = []
+    
     self.reload(manager,xml)
 
+  def destroy(self):
+    for portal in self.portals:
+      portal.portal1 = None
+      portal.portal2 = None
+      portal.portalNode1.removeNode()
+      portal.portalNode2.removeNode()
+      
+    for aabb in self.bounds:
+      children = aabb.cell.getChildren()
+      for i in xrange(children.size()):
+        child = children[i]
+        child.reparentTo(aabb.cell.getParent())
+
+      aabb.cell.removeNode()
+
+    self.camBound = None
+
+
   def reload(self,manager,xml):
+    # Clean up any previous stuff...
+    self.destroy()
+
     # Get the level plugin to load the culling AABB's from...
     levelXML = xml.find('level')
     if levelXML!=None:
@@ -32,9 +56,76 @@ class CullAABB:
     level = manager.get(levelPlugin)
 
     # Load the AABB's into a list of bounds...
-    bounds = []
+    self.bounds = []
     for node in level.getByIsA('CullAABB'):
       low = Point3()
       high = Point3()
       node.calcTightBounds(low,high)
-      bounds.append(AABB(low,high))
+      self.bounds.append(AABB(low,high))
+
+    if xml.find('debug')!=None:
+      print 'Found',len(self.bounds),'bounding boxes for the culling system'
+
+    # Generate a cell for each bounding box...
+    for aabb in self.bounds:
+      aabb.cell = render.attachNewNode('cell')
+      aabb.cell.setPos(aabb.centre[0],aabb.centre[1],aabb.centre[2])
+      aabb.cell.hide()
+
+    # Generate a KD tree of aabb's that is used to quickly work out where a given location is in the culling structure...
+    self.kd = SetAABB(self.bounds)
+
+    # Generate the portal list...
+    self.portals = findPortals(self.bounds)
+    if xml.find('debug')!=None:
+      print 'Found',len(self.portals),'portals for the culling system'
+
+    # Go through and create the portals - a pair for each scenario...
+    for portal in self.portals:
+      portal.portal1 = PortalNode('portal1')
+      portal.portalNode1 = portal.aabb1.cell.attachNewNode(portal.portal1)
+      portal.portal1.setCellIn(portal.aabb1.cell)
+      portal.portal1.setCellOut(portal.aabb2.cell)
+      portal.setupPortal(portal.portal1,portal.portalNode1,False)
+      
+      portal.portal2 = PortalNode('portal2')
+      portal.portalNode2 = portal.aabb2.cell.attachNewNode(portal.portal2)
+      portal.portal2.setCellIn(portal.aabb2.cell)
+      portal.portal2.setCellOut(portal.aabb1.cell)
+      portal.setupPortal(portal.portal2,portal.portalNode2,True)
+
+    # Setup assorted variables...
+    self.camBound = None # AABB that the camera is in.
+
+
+  def camCellUpdate(self,task):
+    # Determine if the camera has moved cell, if so update cell visibility...
+    if self.camBound!=None:
+      if not self.camBound.within(base.camera):
+        newBound = self.kd.within(base.camera)
+        if newBound!=None:
+          self.camBound.cell.hide()
+          self.camBound = newBound
+          self.camBound.cell.show()
+    else:
+      self.camBound = self.kd.within(base.camera)
+      if self.camBound:
+        self.camBound.cell.show()
+    return task.cont
+
+
+  def start(self):
+    self.task = taskMgr.add(self.camCellUpdate,'Culling Updater')
+
+  def stop(self):
+    taskMgr.remove(self.task)
+
+  def cullStatic(self,node):
+    """Given a node path that doesn't move, or has a limited movement range known to be within an entire culling aabb, this adds it to the culling system by reparenting it to the correct cell.
+    If there is no cell it belongs in it reparents it to render as a fallback."""
+    bound = self.kd.within(node)
+    if bound!=None:
+      node.reparentTo(bound.cell)
+      node.setPos(node,-bound.cell.getPos())
+    else:
+      node.reparentTo(render)
