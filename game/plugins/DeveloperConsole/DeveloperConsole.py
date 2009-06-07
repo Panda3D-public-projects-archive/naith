a98c5a187b3ebc2129b0e39e57788d03605c8494
niertie1 <niertie1@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-06-07 09:02:05 +0000

    More pythonic console

--- game/plugins/DeveloperConsole/DeveloperConsole.py
+++ game/plugins/DeveloperConsole/DeveloperConsole.py
@@ -12,28 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pandac.PandaModules import TextNode
+from pandac.PandaModules import TextNode, TextProperties, TextPropertiesManager
 from direct.showbase.DirectObject import DirectObject
 from direct.gui.DirectGui import DirectFrame, DirectEntry
 from direct.gui.OnscreenText import OnscreenText
 import sys, traceback
+import __main__
+from code import InteractiveInterpreter
 
 TEXT_MARGIN = (0.03, -0.06)
 
-class DeveloperConsole(DirectObject):
+class PseudoFile:
+  def __init__(self, write):
+    self.write = write
+  def readline(self): pass
+  def writelines(self, l): map(self.append, l)
+  def flush(self):    pass
+  def isatty(self):   return 1
+
+class DeveloperConsole(InteractiveInterpreter, DirectObject):
   """The name says it all."""
   def __init__(self,manager,xml):
+    sys.stdout = PseudoFile(self.writeOut)
+    sys.stderr = PseudoFile(self.writeErr)
+    tpErr = TextProperties()
+    tpErr.setTextColor(1, 0, 0, 1)
+    TextPropertiesManager.getGlobalPtr().setProperties("err", tpErr)
     self.manager = manager
     font = loader.loadFont("cmss12")
     self.frame = DirectFrame(parent = base.a2dTopCenter, text_align = TextNode.ALeft, text_pos = (-base.getAspectRatio() + TEXT_MARGIN[0], TEXT_MARGIN[1]), text_scale = 0.05, text_fg = (1, 1, 1, 1), frameSize = (-2.0, 2.0, -0.5, 0.0), frameColor = (0, 0, 0, 0.5), text = '', text_font = font)
     self.entry = DirectEntry(parent = base.a2dTopLeft, command = self.command, scale = 0.05, width = 1000.0, pos = (-0.02, 0, -0.48), relief = None, text_pos = (1.5, 0, 0), text_fg = (0.5, 0, 0, 1), rolloverSound = None, clickSound = None, text_font = font)
     self.otext = OnscreenText(parent = self.entry, scale = 1, align = TextNode.ALeft, pos = (1, 0, 0), fg = (1, 1, 1, 1), text = ':', font = font)
-    self.lines = [''] * 8
+    self.lines = [''] * 9
     self.commands = []  # All previously sent commands
     self.cscroll = None # Index of currently navigated command, None if current
     self.command = ''   # Currently entered command
-    self.pure = bool(xml.get('pure'))
+    self.block = ''     # Temporarily stores a block of commands
     self.hide()
+    self.initialized = False
   
   def prevCommand(self):
     if self.hidden: return
@@ -62,42 +78,54 @@ class DeveloperConsole(DirectObject):
     else:
       self.entry.set(self.commands[self.cscroll])
       self.entry.setCursorPosition(len(self.commands[self.cscroll]))
-  
-  def addText(self, lines):
-    if isinstance(lines, list):
-      self.lines += lines
-      self.frame['text'] = '\n'.join(self.lines[-8:])
-    else:
-      self.addText(lines.strip('\n').split('\n'))
-  
+
+  def writeOut(self, line, copy = True):
+    if copy: sys.__stdout__.write(line)
+    lines = line.split('\n')
+    firstline = lines.pop(0)
+    self.lines[-1] += firstline
+    self.lines += lines
+    self.frame['text'] = '\n'.join(self.lines[-9:])
+
+  def writeErr(self, line, copy = True):
+    if copy: sys.__stderr__.write(line)
+    line = '\1err\1%s\2' % line
+    lines = line.split('\n')
+    firstline = lines.pop(0)
+    self.lines[-1] += firstline
+    self.lines += lines
+    self.frame['text'] = '\n'.join(self.lines[-9:])
+
   def command(self, text):
     if not self.hidden:
       self.cscroll = None
       self.command = ''
       self.entry.set('')
       self.entry['focus'] = True
-      self.addText(': ' + text)
-      if len(text) == 0:
-        # Nothing was entered.
-        return
-      if len(self.commands) == 0 or self.commands[-1] != text:
+      self.writeOut(self.otext['text'] + ' ' + text + '\n', False)
+      if text != '' and (len(self.commands) == 0 or self.commands[-1] != text):
         self.commands.append(text)
       
       # Insert plugins into the local namespace
-      manager = self.manager
+      locals = __main__.__dict__
+      locals['manager'] = self.manager
       for plugin in self.manager.named.keys():
-        locals()[plugin] = self.manager.named[plugin]
+        locals[plugin] = self.manager.named[plugin]
       
       # Run it and print the output.
+      if not self.initialized:
+        InteractiveInterpreter.__init__(self, locals = locals)
+        self.initialized = True
       try:
-        output = eval(text)
-        if hasattr(output, '__call__') and not self.pure:
-          output = output()
-        if output != None:
-          self.addText(str(output))
+        if self.runsource(self.block + '\n' + text) and text != '':
+          self.otext['text'] = '.'
+          self.block += '\n' + text
+        else:
+          self.otext['text'] = ':'
+          self.block = ''
       except:
         # Whoops! Print out a traceback.
-        self.addText(traceback.format_exc())
+        self.append(traceback.format_exc())
 
   def toggle(self):
     if self.hidden:
@@ -125,6 +153,8 @@ class DeveloperConsole(DirectObject):
     self.otext.hide()
 
   def destroy(self):
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
     self.ignoreAll()
     self.frame.destroy()
     self.entry.destroy()
