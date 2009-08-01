c52e102d84601d495b690fb5647cfb72ba815fc6
thaines <thaines@886e8bc6-2f61-11de-94d6-134fa40061eb>
2009-08-01 16:04:06 +0000

    Simple video recording, for debugging issue #1

--- game/plugins/Window/Window.py
+++ game/plugins/Window/Window.py
@@ -1,4 +1,4 @@
-# Copyright Reinier de Blois
+# Copyright 2009 Reinier de Blois, Tom SF Haines
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,9 @@
 from pandac.PandaModules import *
 from direct.directbase import DirectStart
 from sys import exit
+import datetime
+
+
 
 class Window:
   """This plugin creates a window for rendering into."""
@@ -22,7 +25,29 @@ class Window:
     base.openDefaultWindow()
     render.setAntialias(AntialiasAttrib.MAuto)
     base.setBackgroundColor(0.0, 0.0, 0.0)
-  
+
+    self.task = None
+
+
   screenshot = lambda self: base.screenshot()
   wireframe  = lambda self: base.toggleWireframe()
 
+
+  def record(self,task):
+    base.screenshot(defaultFilename=False,namePrefix=('video|%s|%04i.jpg'%(self.video,self.frameNum)))
+    self.frameNum += 1
+    return task.cont
+
+  def toggleRecording(self):
+    if self.task==None:
+      self.frameNum = 0
+      self.video = datetime.datetime.now().strftime('%Y-%m-%d|%H:%M:%S')
+      self.task = taskMgr.add(self.record,'RecordFrames')
+      globalClock.setMode(ClockObject.MNonRealTime)
+      globalClock.setFrameRate(25.0)
+      print 'started recording'
+    else:
+      globalClock.setMode(ClockObject.MNormal)
+      taskMgr.remove(self.task)
+      self.task = None
+      print 'ended recording'
