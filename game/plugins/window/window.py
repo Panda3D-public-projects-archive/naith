be70a853efcc681d0dbd9330f540c08b71cb0383
daniel <exodaniel@gmail.com>
2021-05-26 14:58:58 +0200

    future print_function

--- game/plugins/window/window.py
+++ game/plugins/window/window.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import print_function
 
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
